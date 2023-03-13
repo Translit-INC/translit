b4e655a96f49f9aa38da884ad9f09691505467b7
Pranjal-Patel <pranjalpatel9838@gmail.com>
2023-03-13 10:54:37 +0530

    Created functions for starting and ending functions

--- src/ir/mod.rs
+++ src/ir/mod.rs
@@ -1,10 +1,11 @@
 pub use instruction::{Arg, Instruction, InstructionCode};
 pub use types::{Literal, Type, Variable};
 
-use self::types::{Function, Signature};
+use self::{types::{Function, Signature}, error_type::{FunctionError, TranslitError}};
 
 pub mod instruction;
 pub mod types;
+pub mod error_type;
 
 /// IR Builder
 #[derive(Debug, Clone, Default)]
@@ -22,13 +23,32 @@ impl IRBuilder {
         }
     }
 
-    pub fn create_function(&mut self, sig: Signature) -> Function {
-        let f = Function {
-            id: self.funcs.len(),
-            sig,
-        };
+    /// Start a function (every instruction will be placed inside this function till you call end_function)
+    pub fn start_function(&mut self, sig: Signature) -> Result<Function, impl TranslitError> {
+        let lst_fn = self.funcs.last();
+
+        if self.funcs.len() != 0 && lst_fn.unwrap().end.is_none() {
+            return Err(FunctionError::new("Cannot start another function", "A function is already ongoing"));
+        }
+
+        let f = Function::new(self.instructions.len(), sig);
         self.funcs.push(f.clone());
-        f
+
+        Ok(f)
+    }
+
+    pub fn end_function(&mut self, func: &mut Function) -> Result<(), impl TranslitError> {
+        let lst_fn = self.funcs.last();
+
+        if self.funcs.len() != 0 && lst_fn.unwrap().end.is_some() {
+            return Err(FunctionError::new("Cannot end a function", "This function is already ended"));
+        }
+
+        self.instructions.push(Instruction::new(InstructionCode::END, [Arg::NONE, Arg::NONE, Arg::NONE]));
+
+        func.end = Some(self.instructions.len() - 1); // then END instruction we just pushed
+
+        Ok(())
     }
 
     /// Push an instruction into the IR
@@ -48,6 +68,7 @@ impl IRBuilder {
             })),
             _ => panic!("Too many arguments"),
         };
+
         Variable(self.instructions.len() - 1)
     }
 }
