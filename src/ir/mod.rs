8c0a04970a08b758ed0be956865e81ed2ebc099c
Pranjal-Patel <pranjalpatel9838@gmail.com>
2023-03-13 19:57:41 +0530

    removed test

--- src/ir/mod.rs
+++ src/ir/mod.rs
@@ -4,26 +4,5 @@ pub use types::{Block, BlockID, Function, FunctionID, Literal, Signature, Type, 
 pub mod builder;
 pub mod instruction;
 pub mod types;
+
 pub use builder::IRBuilder;
-
-#[cfg(test)]
-mod tests {
-
-    use crate::error::TranslitResult;
-
-    use super::*;
-
-    #[test]
-    /// Starting a function test
-    fn make_function() {
-        let mut builder = IRBuilder::new();
-        let sig = Signature::new(&[], Type::NONE);
-        match builder.make_function(&sig, |builder| {
-            builder.push(InstructionCode::ADD, [])?; // Test
-            TranslitResult::Ok(())
-        }) {
-            Ok(_) => {},
-            Err(e) => println!("{}", e)
-        };
-    }
-}
