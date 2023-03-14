3480d9cda5e1f085ff18c0917cbda4df71640474
Pranjal-Patel <pranjalpatel9838@gmail.com>
2023-03-14 10:14:09 +0530

    added testing for assembly generator

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,30 @@
 #![allow(dead_code)]
+#![allow(unused)]
+
 extern crate num;
 #[macro_use]
 extern crate num_derive;
 
 pub mod error;
 pub mod ir;
-pub mod assembly_generator;
+
+pub(crate) mod assembly_generator;
+
+#[cfg(test)]
+mod tests {
+    use crate::error::{TranslitError, TranslitResult};
+    use crate::ir::{IRBuilder, Signature, Instruction, InstructionCode, Type, Arg, Literal};
+
+    #[test]
+    fn asm_gen() {
+        let mut builder = IRBuilder::new();
+        let main_func = builder.start_function(&Signature::new(&[Type::NONE], Type::NONE));
+
+        builder.push(InstructionCode::ADD, [Arg::Literal(Literal::int8(1)), Arg::Literal(Literal::int8(2))]);
+        builder.push(InstructionCode::SUB, [Arg::Literal(Literal::int8(3)), Arg::Literal(Literal::int8(1))]);
+
+        builder.end_function().unwrap();
+
+        builder.generate_assembly();
+    }
+}
