f424e547b691b5019856b0694e54bec26558eff5
Pranjal-Patel <pranjalpatel9838@gmail.com>
2023-03-14 10:10:03 +0530

    Added main function check

--- src/ir/generator.rs
+++ src/ir/generator.rs
@@ -1,11 +1,22 @@
 use super::builder::IRBuilder;
+use crate::error::{TranslitError, TranslitResult};
+
+/*
+ *
+ *  First Function will be considered as the main function......
+ *
+ */
 
 impl IRBuilder {
-    pub fn generate_assembly(&mut self) {
+    pub fn generate_assembly(&mut self) -> TranslitResult<()> {
         let asm = String::new();
 
-        let functions = self.functions.clone();
-        let instructions = self.instructions.clone();
-        let blocks = self.blocks.clone();
+        if self.functions.len() == 0 {
+            return Err(TranslitError::AssemblyGenerationError("Main function not found".to_string()));
+        }
+
+        self.generate_assembly_nasm_x86_64();
+
+        Ok(())
     }
 }
