7504f1d28a62230da300f675326132c28be59787
Pranjal-Patel <pranjalpatel9838@gmail.com>
2023-04-04 09:58:46 +0530

    marked current as public

--- src/assembly_generator/mod.rs
+++ src/assembly_generator/mod.rs
@@ -1,7 +1,7 @@
 #[cfg(target_arch = "x86_64")] pub mod x86_64;
 #[cfg(target_arch = "arm")] pub mod arm;
 
-#[cfg(target_arch = "x86_64")] use self::x86_64 as current;
-#[cfg(target_arch = "arm")] use self::arm as current;
+#[cfg(target_arch = "x86_64")] pub use self::x86_64 as current;
+#[cfg(target_arch = "arm")] pub use self::arm as current;
 
 pub mod error;
