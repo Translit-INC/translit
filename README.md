0b54d5e44d2e22acfae6cd459a09a2a214f28b85
thatmagicalcat <thatmagicalcat@duck.com>
2024-11-17 17:59:16 +0530

    so empty...

--- README.md
+++
@@ -1,6 +0,0 @@
-# translit
-Translit repo (WIP)
-# TODO
-- [ ] Make the IR.
-- [ ] Add x86_64 assembly translation for every instruction.
-
--- src/assembly_generator/mod.rs
+++
@@ -1,7 +0,0 @@
-#[cfg(target_arch = "x86_64")] pub mod x86_64;
-#[cfg(target_arch = "arm")] pub mod arm;
-
-#[cfg(target_arch = "x86_64")] pub use self::x86_64 as current;
-#[cfg(target_arch = "arm")] pub use self::arm as current;
-
-pub mod error;
